fc30e8cbe73b7832db422d454201d69fd3174b46
Melany Paola Rivera Lores <38965651+MelsLores@users.noreply.github.com>
2025-10-02 16:28:00 -0600

    Initialize Spring Boot project structure
    Add initial Maven configuration, main application class, application properties, and a basic test class for a Spring Boot project using H2 database and JPA. This sets up the foundation for further development.

---
+++ src/main/java/com/googlescholar/GoogleScholarProjectApplication.java
@@ -0,0 +1,12 @@
+package com.googlescholar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleScholarProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleScholarProjectApplication.class, args);
+    }
+}
